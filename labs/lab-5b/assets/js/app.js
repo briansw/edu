0aa64b2a20af36d12e31c806d0fdc5f2c85ec54d
Brian Watterson <briansw@gmail.com>
2018-10-30 11:15:53 -0400

    lab 5

--- labs/lab-5b/assets/js/app.js
+++ labs/lab-5b/assets/js/app.js
@@ -1,22 +1,23 @@
 $('.nav-item').click(function() {
   var button_name = $(this).data('button');
-
-  $('.content').attr('class', 'column content');
-  $('.content').addClass(button_name);
-  $('.nav-item').removeClass('active');
-  $(this).addClass('active');
+  
   $('.about-text').addClass('hide');
-
+  $('.nav-item').removeClass('active');
+  $('.content').attr('class', 'column content');
+  $(this).addClass('active');
+  $('.content').addClass(button_name);
+  
   if (button_name == 'beyonce') {
     $('.slay').removeClass('hide');
     $('audio')[0].play();
   } else if (button_name == 'about') {
     $('.about-text').removeClass('hide');
   }
+
 });
 
 $('.close').click(function() {
   $('.slay').addClass('hide');
-  $('.nav-item').removeClass('active');
   $('audio')[0].pause();
+  $('.nav-item').removeClass('active');
 });
